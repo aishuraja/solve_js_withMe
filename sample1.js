081d8154e4eee8f4372c6cf705af7ac0c0a3ecba
aishuraja <aishwaryaraja@crc-dot1x-nat-10-239-95-242.bu.edu>
2024-04-14 19:37:53 -0400

    basic calwith js

---
+++ sample1.js
@@ -0,0 +1,8 @@
+var a = 10
+var b = 20
+function add()
+{
+    c= a+ b
+    console.log("the val of c is:"+c)
+}
+add()
---
+++ sample2.js
@@ -0,0 +1,10 @@
+
+function area(l, b)
+{
+    var a = l*b
+    console.log("area is:"+a)
+
+}
+var length = 10
+var breadth = 20
+area(10,20)
